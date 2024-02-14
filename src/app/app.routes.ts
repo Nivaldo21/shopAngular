198a8a0831ab8ad37c593bdcfd311b09e01f1e40
Nivaldo21 <djnivis@gmail.com>
2024-02-14 11:50:59 -0600

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SignUpComponent } from './pages/auth/sign-up/sign-up.component';
+import { LogInComponent } from './pages/auth/log-in/log-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', loadComponent: ()=> HomeComponent, pathMatch: 'full'}, //home\
+    {path: 'sign-up', loadComponent: ()=> SignUpComponent},
+    {path: 'log-in', loadComponent: ()=> LogInComponent},
+    { path: '**', redirectTo: '' } // Ruta por defecto en caso de que no coincida con ninguna ruta definida
+];
