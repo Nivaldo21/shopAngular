b37bea8a34c553af2808bf803f7b68ea5b16720f
Nivaldo21 <djnivis@gmail.com>
2024-03-01 12:27:27 -0600

    feat: add cart component with suscribe to observable cart

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { CartServiceService } from '../../services/cart-service.service';
+import Product from '../../interfaces/product.interface';
+import { CommonModule } from '@angular/common';
+import {MatListModule} from '@angular/material/list';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { Cart } from '../../interfaces/cart.interface';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,MatDividerModule,MatListModule,MatIconModule,MatGridListModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
 
+  constructor(private cartService:CartServiceService){}
+
+  cartItems:Product[] = [];
+  ngOnInit(): void {
+    this.cartService.cartList$.subscribe((cartItems:Product[])=>{
+      this.cartItems = cartItems;
+    });
+  }
 }
