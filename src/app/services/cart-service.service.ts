f0e82f5cecd5c89f1d0dc05903fc70d960d50671
Nivaldo21 <djnivis@gmail.com>
2024-03-01 10:24:48 -0600

    feat: add cart service with interface

---
+++ src/app/services/cart-service.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import Product from '../interfaces/product.interface';
+import { BehaviorSubject,Observable } from 'rxjs';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartServiceService {
+
+  private _cartList: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>([]);
+
+  constructor(private _snackBar: MatSnackBar) {}
+
+  get cartList$(): Observable<Product[]> {
+    return this._cartList.asObservable();
+  }
+
+  addToCart(product: Product): void {
+    const updatedCartList = [...this._cartList.value, product];
+    this._cartList.next(updatedCartList);
+    this._snackBar.open("New Item in your Cart ✅ ",'',{duration: 5000});
+  }
+
+
+}
